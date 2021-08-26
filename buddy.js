1ff5744ee26e475dc42ed8987a7b41ed9f29b704
sksatu1 <satueee18@gmail.com>
2021-08-27 01:11:35 +0600

    4th

--- buddy.js
+++ buddy.js
@@ -1,7 +1,18 @@
 const loadBuddies = () => {
     fetch('https://randomuser.me/api/?results=5')
         .then(res => res.json())
-        .then(data => console.log(data))
+        .then(data => displayBuddies(data))
 }
-
-const displayBuddies
+loadBuddies();
+const displayBuddies = data => {
+    const buddies = data.results;
+    const buddiesContainer = document.getElementById('buddies');
+    for (const buddy of buddies) {
+        // const name = buddy.name.first;
+        const p = document.createElement('p');
+        p.innerText = `name: ${buddy.name.title} ${buddy.name.first}  ${buddy.name.last} 
+        email: ${buddy.email}
+        `;
+        buddiesContainer.appendChild(p);
+    }
+}
