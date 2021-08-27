4f5fa576ca13910ad99b6b8279796541be93a17c
sksatu1 <satueee18@gmail.com>
2021-08-27 12:35:54 +0600

    5th commit

---
+++ countries.js
@@ -0,0 +1,50 @@
+const loadCountries = () => {
+    fetch('https://restcountries.eu/rest/v2/all')
+        .then(res => res.json())
+        .then(data => displayCountries(data))
+}
+loadCountries();
+
+const displayCountries = countries => {
+    /* for (country of countries) {
+        console.log(country);
+    } */
+    const countriesDiv = document.getElementById('countries');
+    countries.forEach(country => {
+        const div = document.createElement('div');
+        div.classList.add('country');
+
+        div.innerHTML = `
+            <h3>${country.name}</h3>
+            <p>${country.capital}</p>
+            <button onclick="loadCountryByName('${country.name}')">Details</button>
+        `;
+        countriesDiv.appendChild(div);
+        /* const h3 = document.createElement('h3');
+        h3.innerText = country.name;
+        div.appendChild(h3);
+
+        const p = document.createElement('p');
+        p.innerText = country.capital;
+        div.appendChild(p);
+        countriesDiv.appendChild(div); */
+    });
+}
+
+const loadCountryByName = name => {
+    // console.log(name);
+    const url = `https://restcountries.eu/rest/v2/name/${name}`
+    fetch(url)
+        .then(response => response.json())
+        .then(data => displayCountryDetail(data[0]))
+}
+
+const displayCountryDetail = country => {
+    console.log(country);
+    const countryDiv = document.getElementById('country-div');
+    countryDiv.innerHTML = `
+    <h5>${country.name}</h5>
+    <p>${country.population}</p>
+    <img width="200px" src="${country.flag}">
+    `;
+}
