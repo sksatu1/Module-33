f428496d1ba518960d3d37b3d4762cae848e88ed
sksatu1 <satueee18@gmail.com>
2021-08-27 15:27:38 +0600

    6th

--- mealdb.js
+++ mealdb.js
@@ -10,5 +10,26 @@ const searchFood = () => {
 
     fetch(url)
         .then(res => res.json())
-        .then(data => console.log(data.meals))
+        .then(data => displaySearchResult(data.meals))
+}
+
+
+const displaySearchResult = meals => {
+    const searchResult = document.getElementById('search-result');
+    meals.forEach(meal => {
+        console.log(meal);
+
+        const div = document.createElement('div');
+        div.classList.add('col');
+        div.innerHTML = `
+        <div class="card">
+                <img src="${meal.strMealThumb}" class="card-img-top" alt="">
+                <div class="card-body">
+                    <h5 class="card-title">${meal.strMeal}</h5>
+                    <p class="card-text">${meal.strInstructions.slice(0, 100)}</p>
+                </div>
+        </div>
+        `;
+        searchResult.appendChild(div)
+    });
 }
